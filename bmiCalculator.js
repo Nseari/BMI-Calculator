6d4b02a7d80ce5742a7f5bce920c328ea5d21580
Nseari <40863132+Nseari@users.noreply.github.com>
2022-10-24 21:06:52 +0100

    Create bmiCalculator.js

---
+++ bmiCalculator.js
@@ -0,0 +1,18 @@
+function bmiCalculator (weight, height) {
+    var bmi = weight/Math.pow(height,2);
+
+    
+    if (bmi<=18.5){
+        bmi= "Your BMI is " + bmi + ", so you are underweight.";
+    }
+    if (bmi >= 18.5 && bmi <= 24.9){
+        bmi= "Your BMI is " + bmi + ", so you have a normal weight.";
+    }
+    
+    if (bmi>24.9) {
+    bmi = "Your BMI is " + bmi  + ", so you are overweight.";
+    }
+return bmi;
+
+}
+bmiCalculator(40,2);
