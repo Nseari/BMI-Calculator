49658a4942af8adf418fb0336fe2b3ec53ec9ae5
Nseari <40863132+Nseari@users.noreply.github.com>
2022-10-25 02:48:08 +0100

    Update bmiCalculator.js

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -5,11 +5,11 @@ function bmiCalculator (weight, height) {
     if (bmi<=18.5){
         bmi= "Your BMI is " + bmi + ", so you are underweight.";
     }
-    if (bmi >= 18.5 && bmi <= 24.9){
+    else if (bmi >= 18.5 && bmi <= 24.9){
         bmi= "Your BMI is " + bmi + ", so you have a normal weight.";
     }
     
-    if (bmi>24.9) {
+    else if (bmi>24.9) {
     bmi = "Your BMI is " + bmi  + ", so you are overweight.";
     }
 return bmi;
